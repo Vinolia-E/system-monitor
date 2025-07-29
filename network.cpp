3c5df8f708c8a6fe6e9a5127a138007f12d4ea2a
Vinolia-E <vinolia.b.19@gmail.com>
2025-07-29 12:47:06 +0300

    Implemented  to list all active IPv4 interfaces using . Extracts interface names and IP addresses, storing them in a  struct.

--- network.cpp
+++ network.cpp
@@ -1,1 +1,28 @@
 #include "header.h"
+#include <cstring>
+#include <cstdio>
+
+Networks getNetworks()
+{
+    Networks networks;
+    struct ifaddrs *ifaddr, *ifa;
+    
+    if (getifaddrs(&ifaddr) == -1) {
+        return networks;
+    }
+    
+    for (ifa = ifaddr; ifa != nullptr; ifa = ifa->ifa_next) {
+        if (ifa->ifa_addr == nullptr) continue;
+        
+        if (ifa->ifa_addr->sa_family == AF_INET) {
+            IP4 ip4;
+            ip4.name = strdup(ifa->ifa_name);
+            struct sockaddr_in* addr_in = (struct sockaddr_in*)ifa->ifa_addr;
+            inet_ntop(AF_INET, &(addr_in->sin_addr), ip4.addressBuffer, INET_ADDRSTRLEN);
+            networks.ip4s.push_back(ip4);
+        }
+    }
+    
+    freeifaddrs(ifaddr);
+    return networks;
+}
